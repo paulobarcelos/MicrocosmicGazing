f0cfee7477c7ddae33d563251cbb5ebeee32b102
Paulo Barcelos <paulo@azucrina.org>
2021-05-19 20:55:02 +0200

    updated addons

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,33 +2,57 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    cam.initGrabber(640, 480);
+    ofSetVerticalSync(true);
+    ofSetFullscreen(true);
+
+    for (int i = 0; i<NUM_CAMERAS; i++) {
+        cam[i].setDeviceID(i);
+        cam[i].initGrabber(1280, 720);
+    }
     
-    /**
-     Always this order:
-     - setup
-     - allocate
-     - registerPatch
-     - loadSettings
-     **/
+    for (int i = 0; i<NUM_VIDEOS; i++) {
+        video[i].load("videos/" + ofToString(i) + ".mov");
+        video[i].setLoopState(OF_LOOP_NORMAL);
+        video[i].setVolume(0);
+        video[i].play();
+    }
     
-    manager.setup(1);
-    manager.allocate(640, 480);
-    OFX_PATCHES_REGISTER_ALL_EFFECTS(manager);
-    manager.loadSettings();
+    for (int i = 0; i<NUM_EFFECTS; i++) {
+        effect[i].setup(NUM_CAMERAS + NUM_VIDEOS, "effect" + ofToString(i));
+        effect[i].allocate(1280, 720);
+        OFX_PATCHES_REGISTER_ALL_EFFECTS(effect[i]);
+        effect[i].loadSettings();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    cam.update();
-    if(cam.isFrameNew()){
-        manager.setTexture(cam);
-        manager.update();
+    for (int i = 0; i<NUM_CAMERAS; i++) {
+        cam[i].update();
+        if(cam[i].isFrameNew()){
+            for (int j = 0; j<NUM_EFFECTS; j++) {
+                effect[j].setTexture(cam[i], i);
+                effect[j].update();
+            }
+            
+        }
+        
+    }
+    for (int i = 0; i<NUM_VIDEOS; i++) {
+        video[i].update();
+        if(video[i].isFrameNew()){
+            for (int j = 0; j<NUM_EFFECTS; j++) {
+                effect[j].setTexture(video[i], NUM_CAMERAS + i);
+                effect[j].update();
+            }
+        }
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    manager.drawGUI();
+    for (int j = 0; j<NUM_EFFECTS; j++) {
+        effect[j].drawGUI();
+    }
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#define NUM_CAMERAS 3
+#define NUM_VIDEOS 2
+#define NUM_EFFECTS 5
+
 #include "ofMain.h"
 #include "ofxPatches.h"
+
 
 class ofApp : public ofBaseApp{
 
@@ -10,6 +15,7 @@ class ofApp : public ofBaseApp{
         void update();
         void draw();
 
-        ofVideoGrabber cam;
-        ofxPatches::Manager manager;
+        ofVideoGrabber cam[NUM_CAMERAS];
+        ofVideoPlayer video[NUM_VIDEOS];
+        ofxPatches::Manager effect[NUM_EFFECTS];
 };
